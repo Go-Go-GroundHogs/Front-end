68702730b3557aa9720415c4a158924084bee2fb
Nick Reshetnikov <nick.reshetnikov@gmail.com>
2021-09-28 02:39:46 -0700

    implement in intro, benefits, and club content sections
    - mostly leverage mdx files to arrange both text and components

--- src/components/content-block.js
+++ src/components/content-block.js
@@ -1,9 +1,9 @@
 import { Col, Row } from "react-bootstrap"
 import React from "react"
 
-const ContentBlock = ({ id, className, children }) => {
+const ContentBlock = ({ id, className = "", children }) => {
   return (
-    <Row id={id} className="content-block border-bottom py-5">
+    <Row id={id} className={`content-block border-bottom py-5 ${className}`}>
       <Col>
         {children}
       </Col>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,64 +3,42 @@ import React from "react"
 
 import BackgroundBlock from "../components/background-block"
 import CallToAction from "../components/call-to-action"
-import CallToActionBanner from "../components/call-to-action-banner"
-import Carousel3D from "../components/carousel-3d"
 import ContentBlock from "../components/content-block"
-import EmailForm from "../components/email-form"
 import Layout from "../components/layout"
 import RasterImg from "../components/raster-img"
-import TextImgSection from "../components/text-img-section"
 
-
+import BenefitsMDX from "../content/benefits"
 import CallToActionMDX from "../content/call-to-action"
+import ClubMDX from "../content/club"
 import IntroMDX from "../content/intro"
-import Info1MDX from "../content/info1"
-import TypesMDX from "../content/types"
-import LiveGirlsMDX from "../content/live-girls"
-import BenefactorsMDX from "../content/benefactors"
-import GentlemansClubMDX from "../content/gentlemans-club"
 import RoadmapMDX from "../content/roadmap"
-import TeamMDX from "../content/team"
-
-const CAROUSEL_IMAGES = [
-  '241180858_210173591097713_3254127091026144355_n.jpg',
-  '242413246_198117819082822_2512234783661161239_n.png',
-  '242662278_351131996792436_3164517192214611283_n.png',
-  '242662684_556932562187658_793506315002072693_n.png',
-  '242690115_389067659501615_205551625228433586_n.png',
-  '242877457_159365253038369_8722042843496788759_n.png'
-]
 
 const SECTIONS = [
   { id: "intro",
     subSections: [
       {
-        component: Carousel3D,
-        attr: { images: CAROUSEL_IMAGES }
+        component: IntroMDX
       }
     ]
   },
-  { id:"gentlemans-club",
+  { id:"benefits",
     subSections: [
       {
-        component: TextImgSection,
-        attr: {markdown: GentlemansClubMDX, img: "24-small", hColors: ['#7030A0', '#FFC000']}
+        component: BenefitsMDX,
       }
     ]
   },
-  { id:"roadmap",
+  { id:"club",
     subSections: [
       {
-        component: TextImgSection,
-        attr: {markdown: RoadmapMDX, img: "7-small", hColors: ['#FF99CC']}
+        component: ClubMDX,
       }
     ]
   },
-  { id:"team",
+  { id: "roadmap",
     subSections: [
       {
-        component: TextImgSection,
-        attr: {markdown: TeamMDX, img: "9-small", hColors: ['#FFFF00']}
+        component: RoadmapMDX
       }
     ]
   }
@@ -68,19 +46,18 @@ const SECTIONS = [
 
 const HomePage = () => {
   return (
-    <Layout light title="Harems">
+    <Layout light title="Gogo Groundhogs">
       <BackgroundBlock className="d-flex justify-content-center" src="home-bg" minHeight="100vh">
         <CallToAction
           imgLeft={"Groundhog Profile"}
           imgRight={"Groundhog Scope"}
           markdown={CallToActionMDX} />
       </BackgroundBlock>
-      <CallToActionBanner className="black-bg border-bottom border-top" text={<h1>Get the Latest</h1>} cta={<EmailForm id="top-email-form" hideLabel />} ctaFirst />
-      <Container fluid className="black-bg pt-4 px-4 text-left">
+      <Container fluid className="main-content-wrapper pt-4 px-4 text-left">
         {
           SECTIONS.map((section) => {
             return(
-              <ContentBlock id={section.id} key={section.id}>
+              <ContentBlock id={section.id} key={section.id} className={section.className}>
                 {
                   section.subSections.map((subSection, i) => {
                     const Section = subSection.component;
